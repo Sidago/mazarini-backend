e33ce50aa7d6abc31a2af65df556514b500b0e79
Khaledr001 <khaledrazu01@gmail.com>
2026-08-19 20:34:32 +0400

    refactor: update API schema types to support arrays and string inputs and initialize pnpm workspace configuration

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -495,7 +495,7 @@ export interface TimelineMilestone extends Struct.ComponentSchema {
 }
 
 declare module '@strapi/strapi' {
-  export module Public {
+  export namespace Public {
     export interface ComponentSchemas {
       'award.challenge-card': AwardChallengeCard;
       'award.text-column': AwardTextColumn;
